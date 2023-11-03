55246e8eecc395aa4fc97580c5e8fe8ec6e01ab1
hyejin-0 <hj111800@naver.com>
2023-11-04 02:57:42 +0900

    5

--- main.c
+++ main.c
@@ -3,33 +3,48 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-#define SIZE 4
+#define ROWS 3
+#define COLS 3
 
-void square_array(int a[], int size)
+void addMatrix(int A[][COLS], int B[][COLS], int C[][COLS])
 {
-	int i;
+	//C = A + B : for loop
+	int i, c;
 	
-	for (i=0;i<size;i++)
-	  a[i] = a[i] * a[i];
+	for (i=0;i<3;i++)
+	{
+		for (c=0;c<3;c++)
+		C[i][c] = A[i][c] + B[i][c];
+	}
 }
 
-void print_array(int a[], int size)
+void printMatrix(int C[][COLS])
 {
-	int i;
+	//for loop - A의 요소를 각자 출력 
+	int i, c;
 	
-	for (i=0;i<size;i++)
-	  printf("%3d", a[i]);
-	printf("\n");
+	for (i=0;i<3;i++)
+	{
+		for(c=0;c<3;c++)
+		  printf("%d ", C[i][c]);
+		printf("\n");
+	}
 }
 
 int main(int argc, char *argv[]) 
 {
-	int list[SIZE] = {1, 2, 3, 4};
+	int A[ROWS][COLS] = {
+		{2,3,0},
+		{8,9,1},
+		{7,0,5}};
+	int B[ROWS][COLS]= {
+		{1,0,0},
+		{0,1,0},
+		{0,0,1}};
+	int C[ROWS][COLS];
 	
-	print_array(list, SIZE);
-	square_array(list, SIZE);
-	print_array(list, SIZE);
-	
+	addMatrix(A,B,C);
+	printMatrix(C);
 
 	return 0;
 }
